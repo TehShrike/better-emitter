0661e920c13b9d48184a840efec593d336400fe4
Ben Stevinson <stevinson.ben@gmail.com>
2018-12-13 15:03:18 -0600

    Add explicit any[] type to emitter args
    Ensures better-emitter can be compiled when strict mode and/or noImplicitAny mode is enabled.

--- index.d.ts
+++ index.d.ts
@@ -4,7 +4,7 @@ type Unlisten = () => void
 interface Emitter {
 	on(event: string, listener: Listener): Unlisten,
 	once(event: string, listener: Listener): Unlisten,
-	emit(event: string, ...args): void
+	emit(event: string, ...args: any[]): void
 }
 
 declare function makeEmitter<Object>(object?: Object): Emitter & Object
