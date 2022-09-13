9d3c3da44c70b1227e1b7c42ad67944ffbbfe403
Josh Duff <me@JoshDuff.com>
2022-09-13 11:35:17 -0500

    Remove type assertions, which enables non-string event keys
    Why not use Symbols if you feel like it?

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 const keyMaster = require(`key-master`)
 
-const assertType = (name, value, expectedType) => {
-	const actualType = typeof value
-	if (actualType !== expectedType) {
-		throw new Error(`Expected ${ name } to be ${ expectedType } but it was ${ actualType }`)
-	}
-}
-
 const freshMap = () => keyMaster(() => new Map())
 
 module.exports = function createEmitter(emitter = Object.create(null)) {
@@ -14,9 +7,6 @@ module.exports = function createEmitter(emitter = Object.create(null)) {
 	let nextId = 0
 
 	emitter.on = (event, listener) => {
-		assertType(`event`, event, `string`)
-		assertType(`listener`, listener, `function`)
-
 		const id = (nextId++).toString()
 		const listeners = eventsToListeners.get(event)
 		listeners.set(id, listener)
@@ -31,9 +21,6 @@ module.exports = function createEmitter(emitter = Object.create(null)) {
 	}
 
 	emitter.once = (event, listener) => {
-		assertType(`event`, event, `string`)
-		assertType(`listener`, listener, `function`)
-
 		const unsubscribe = emitter.on(event, (...args) => {
 			listener(...args)
 			unsubscribe()
@@ -43,8 +30,6 @@ module.exports = function createEmitter(emitter = Object.create(null)) {
 	}
 
 	emitter.emit = (event, arg) => {
-		assertType(`event`, event, `string`)
-
 		const listeners = eventsToListeners.get(event)
 		for (const listener of listeners.values()) {
 			listener(arg)
